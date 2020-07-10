57488b8794fb9005d71b6de658cc38fbc10cfba1
the.cold.moon <lyliu013@gmail.com>
2020-07-10 14:23:30 -0400

    submission

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,50 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.history = []
+        self.ptr = -1
 
     def add(self, num: int):
+        self.value += num
+        pointer = self.ptr
+
+        if self.is_index(pointer+1):
+            self.history.insert(pointer+1,num)
+        else :
+            self.history.append(num)
+        
+        self.ptr += 1
         pass
 
     def subtract(self, num: int):
-        pass
+        self.add(-num)
 
     def undo(self):
+        pointer = self.ptr
+        if 0 <= pointer :
+            self.value -= self.history[pointer]
+            self.ptr -= 1
         pass
 
     def redo(self):
+        pointer = self.ptr
+        if self.is_index(pointer+1):
+            self.value += self.history[pointer+1]
+            self.ptr += 1
         pass
 
     def bulk_undo(self, steps: int):
+        for x in range(steps):
+            self.undo()
         pass
 
     def bulk_redo(self, steps: int):
+        for x in range(steps):
+            self.redo()
         pass
+
+    def is_index(self, num:int):
+        if 0 <= num < len(self.history):
+            return True
+        return False
+
